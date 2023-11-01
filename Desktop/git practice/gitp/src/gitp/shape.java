19fb55f0608aec7ff33da31f01d1dfee980213c0
manioshaikh <manio.shaikh04@gmail.com>
2023-11-01 11:34:30 +0500

    another modified

--- Desktop/git practice/gitp/src/gitp/shape.java
+++ Desktop/git practice/gitp/src/gitp/shape.java
@@ -1,5 +1,11 @@
 package gitp;
-
-public class shape {
-
+public class shape{
+    public void circle(){
+       System.out.println("circle is made");
+    }
+    public void square(){
+        System.out.println("square is made");
+     }
+    public static void main(String[] args) {
+    }
 }
